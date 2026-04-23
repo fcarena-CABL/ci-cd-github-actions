979680971d949995b95c1aca3b6e6175bc5697d2
ponofa <ponofa@14demarzo.fin.ec>
2026-04-23 23:00:31 +0000

    Nuevo endpoint al api

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 
+
 app.get("/", (req, res) => {
     res.status(200).json({ message: "API Funcionando" });
 })
 
 app.get("/clients", (req, res) => {
     res.status(200).json({ message: "Ok", clients: ["uno","dos"]  });
+})
+
+app.get("/not_found", (req, res) => {
+    res.status(404).json({ message: "Error not found" });
 })
 
 module.exports = app;
